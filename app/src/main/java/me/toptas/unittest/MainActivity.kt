d3959829607617e5f15a667934dbb3ce36021232
faruktoptas <faruktoptas@gmail.com>
2019-03-03 14:19:18 +0300

    Remove part 1 from master

--- app/src/main/java/me/toptas/unittest/MainActivity.kt
+++ app/src/main/java/me/toptas/unittest/MainActivity.kt
@@ -2,27 +2,12 @@ package me.toptas.unittest
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity(), LoginView {
+class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val presenter = LoginPresenter(this)
-        btnLogin.setOnClickListener {
-            presenter.login("user", "pass")
-        }
-    }
-
-    override fun onLoginSuccess() {
-        Log.i("test", "Login success")
-    }
-
-    override fun onLoginFail(e: Throwable) {
-        Log.i("test", "Login fail")
 
     }
 }
